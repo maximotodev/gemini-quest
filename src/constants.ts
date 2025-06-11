3b086fcc858493a373bba8004b69e0acf41177c0
maximotodev <maximotodev@proton.me>
2025-06-11 17:39:47 -0400

    Fix API_BASE_URL

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 export const TOTAL_QUESTIONS = 10;
 export const TIME_PER_QUESTION = 20; // Seconds
 export const ANSWER_FEEDBACK_DURATION = 2000; // Milliseconds (2 seconds)
-export const API_BASE_URL = "http://127.0.0.1:5000"; // Or your deployed backend URL
+export const API_BASE_URL = "https://gemini-quest.onrender.com"; // Or your deployed backend URL
 export const categories = [
   "General Knowledge",
   "Science",
